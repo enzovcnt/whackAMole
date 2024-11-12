10f0d54be019b252e66a8e038ebc52c8e3537cce
enzo <enzovcnt@gmail.com>
2024-11-12 20:09:53 +0100

    mole go out by the correct box

--- js/main.js
+++ js/main.js
@@ -1,13 +1,26 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+let lastHole;
 
-function randTime(min, max){
+function randomTime(min, max){
     return Math.round(Math.random() * (max - min) + min);
 }
 
 function randomHole(holes){
     const idx = Math.floor(Math.random() * holes.length);
     const hole = holes[idx];
-    console.log(hole);
+    if (hole === lastHole){
+        console.log('non mec arrête');
+        return randomHole(holes);
+    }
+
+    lastHole = hole;
+    return hole;
+}
+
+function peep(){
+    const time = randomTime(200, 1000);
+    const hole = randomHole(holes);
+    hole.classList.add('up');
 }
