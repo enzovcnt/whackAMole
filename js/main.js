ca088c65e7212c93421696c3f8ccc6eeb1232465
enzo <enzovcnt@gmail.com>
2024-11-12 20:29:42 +0100

    detect when we bonk the mole

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
 let lastHole;
+timeUp = false;
 
 function randomTime(min, max){
     return Math.round(Math.random() * (max - min) + min);
@@ -25,5 +26,19 @@ function peep(){
     hole.classList.add('up');
     setTimeout(() => {
         hole.classList.remove('up');
+        if(!timeUp) peep();
     }, time);
 }
+
+function startGame(){
+    scoreBoard.textContent= 0;
+    timeUp = false;
+    peep();
+    setTimeout(() => timeUp = true, 10000);
+}
+
+function bonk(e){
+    console.log(e);
+}
+
+moles.forEach(mole => mole.addEventListener('click', bonk));
