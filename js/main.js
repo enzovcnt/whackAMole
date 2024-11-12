663f9a6e30e224e0bd1779fe6e76369369c22cb3
enzo <enzovcnt@gmail.com>
2024-11-12 19:38:09 +0100

    randTime give me random number in the console

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-alert("Hello World!");
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+
+function randTime(min, max){
+    return Math.round(Math.random() * (max - min) + min);
+}
